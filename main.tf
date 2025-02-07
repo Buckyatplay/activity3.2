b37b4d0003c5f1d0ea383e85fa142b2dee5fd7cc
Buckyatplay <rvf_7585@hotmail.com>
2025-02-07 12:50:55 +0800

    try4

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "ap-southeast-1"
-}
-
 terraform {
   backend "s3" {
     bucket = "sctp-ce8-tfstate"
@@ -13,10 +9,17 @@ terraform {
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix = split("/", "${data.aws_caller_identity.current.arn}")[1]
+  name_prefix = split("/", data.aws_caller_identity.current.arn)[1]
   account_id  = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {
   bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
 }
+
+terraform {
+  required_version = ">= 1.0" 
+}
+
+
+
