fc138d76711c4cad8c31abb0956e39bdc16d0708
Buckyatplay <rvf_7585@hotmail.com>
2025-02-08 10:04:46 +0800

    try25

--- main.tf
+++ main.tf
@@ -18,9 +18,9 @@ locals {
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  #checkov:skip=CKV_AWS_62:Suppress
-  #checkov:skip=CKV_AWS_6:Suppress
-  #checkov:skip=CKV_AWS_61:Suppress
+  #checkov:skip=CKV2_AWS_62:Suppress
+  #checkov:skip=CKV2_AWS_6:Suppress
+  #checkov:skip=CKV2_AWS_61:Suppress
   #checkov:skip=CKV_AWS_21:Suppress
   #checkov:skip=CKV_AWS_18:Suppress
   #checkov:skip=CKV_AWS_144:Suppress
